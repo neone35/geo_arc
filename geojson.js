fc3d24b7f474f20e01e798f995195c85b8fa288e
Artur Maslov <aartasas@gmail.com>
2016-12-23 19:51:06 +0200

    1.0
    Initial version

---
+++ geojson.js
@@ -0,0 +1,39 @@
+var someFeatures = [{
+        "type": "Feature",
+        "properties": {
+            "name": "Vilnius Airport",
+            "show_on_map": true,
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [25.28, 54.63]
+        }
+    }, 
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Kennedy Airport",
+            "show_on_map": true,
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [-73.77, 40.64]
+        }
+    },
+    // {
+    //     "type": "Feature",
+    //     "properties": {
+    //         "name": "First Lines",
+    //         "show_on_map": true
+    //     },
+    //     "geometry": {
+    //         "type": "LineString",
+    //         "coordinates": [[-100, 40], [-107, 45], [-110, 55]]
+    //     }
+    // }
+    ];
+    // var myStyle = {
+    //     "color": "#000000",
+    //     "weight": 5,
+    //     "opacity": 0.65
+    // };
